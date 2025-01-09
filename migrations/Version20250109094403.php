32a8090cf53b3d6e5ef2d4c74d8c9dfc39628377
Temayoo <Matt.yo95280@gmail.com>
2025-01-09 10:48:11 +0100

    Make User Table

--- migrations/Version20250109094403.php
+++ migrations/Version20250109094403.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250108203603 extends AbstractMigration
+final class Version20250109094403 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,6 +23,7 @@ final class Version20250108203603 extends AbstractMigration
         $this->addSql('CREATE TABLE book (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, category_id BIGINT NOT NULL, pages INT NOT NULL, publication_date DATETIME NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE book_read (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, book_id BIGINT NOT NULL, rating NUMERIC(5, 2) DEFAULT NULL, description LONGTEXT DEFAULT NULL, is_read TINYINT(1) NOT NULL, cover VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
@@ -31,5 +32,6 @@ final class Version20250108203603 extends AbstractMigration
         $this->addSql('DROP TABLE book');
         $this->addSql('DROP TABLE book_read');
         $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE user');
     }
 }
